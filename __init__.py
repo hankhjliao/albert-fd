eb6b959c803ce8a2ad42cbb32ed4d2c48695e2b2
hankliao87 <hankliao87@gmail.com>
2023-01-12 17:18:02 +0800

    Update python api to version 0.5

--- __init__.py
+++ __init__.py
@@ -1,31 +1,57 @@
 # -*- coding: utf-8 -*-
-"""Find files with fd
-Synopsis: <trigger> <expression>"""
 
 import os
 import subprocess
 
 from albert import *
 
-__title__ = "Find file with fd"
-__version__ = "0.4.0"
-__triggers__ = "f "
-__authors__ = ["hankliao87"]
-__exec_deps__ = ["fd"]
+__doc__ = """
+Find files with fd
+Synopsis: `<trigger> <expression>`
+"""
+
+md_iid = "0.5"
+md_version = "0.1"
+md_name = "Find Files"
+md_description = "Find files with fd"
+md_license = "MIT"
+md_url = "https://github.com/hankliao87/albert-fd"
+md_maintainers = "@hankliao87"
+md_bin_dependencies = ["fd"]
+
+trigger = "f "
 
 SEARCH_LIMIT = 20
 
-iconPath = iconLookup(["preferences-system-search", "system-search" "search", "text-x-generic"])
 
-def handleQuery(query):
-    if query.isTriggered:
+class Plugin(QueryHandler):
+    iconPath = ["xdg:preferences-system-search", "xdg:system-search", "xdg:search", "xdg:text-x-generic"]
+
+    def id(self):
+        return __name__
+
+    def name(self):
+        return md_name
+
+    def description(self):
+        return md_description
+
+    def synopsis(self) -> str:
+        return "<name>"
+
+    def defaultTrigger(self) -> str:
+        return trigger
+
+    def handleQuery(self, query):
+
         query_string = query.string.strip()
 
         if len(query_string) < 3:
-            item = Item(id=__title__, icon=iconPath)
+            item = Item(id=__name__, icon=self.iconPath)
             item.text = "Enter more than three characters to search"
             item.subtext = ""
-            return item
+            query.add(item)
+            return
         else:
             try:
                 proc_result = subprocess.run(['fd', '-L', query_string, '--max-results', str(SEARCH_LIMIT), '/home'], stdout=subprocess.PIPE, text=True, timeout=10)
@@ -33,31 +59,50 @@ def handleQuery(query):
                 results = proc_result.stdout
                 results = results.splitlines()
             except subprocess.TimeoutExpired as ex:
-                item = Item(id=__title__, icon=iconPath)
+                item = Item(id=__name__, icon=self.iconPath)
                 item.text = "Timeout"
                 item.subtext = "Error: " + str(ex)
-                return item
+                query.add(item)
+                return
             except Exception as ex:
-                item = Item(id=__title__, icon=iconPath)
+                item = Item(
+                    id=__name__,
+                    icon=self.iconPath,
+                    actions=[
+                        Action(
+                            id="copy-clipboard",
+                            text="Copy error message to clipboard",
+                            callable=lambda u=str(ex): setClipboardText(u)),
+                        Action(
+                            id="copy-repo-url",
+                            text="Copy repo url to clipboard",
+                            callable=lambda: setClipboardText(md_url)),
+                    ]
+                )
                 item.text = "Error: " + str(ex)
-                item.subtext = "Please create an issue in https://github.com/hankliao87/albert-fd"
-                item.addAction(ClipAction(text="Copy error message to clipboard", clipboardText=str(ex)))
-                item.addAction(ClipAction(text="Copy repo url to clipboard", clipboardText="https://github.com/hankliao87/albert-fd"))
-                return item
+                item.subtext = f"Please create an issue in {md_url}"
+                query.add(item)
+                return
 
             if len(results) == 0:
-                item = Item(id=__title__, icon=iconPath)
+                item = Item(id=__name__, icon=self.iconPath)
                 item.text = "Not Found"
                 item.subtext = query_string
-                return item
+                query.add(item)
+                return
 
-            items = []
             for result in results:
-                item = Item(id=__title__, icon=iconPath)
                 result = result[:-1] if result.endswith('/') else result
-                item.text = os.path.basename(result)
-                item.subtext = result
-                item.addAction(UrlAction("Open", "file://%s" % result))
-                #item.addAction(ProcAction(text="Open", commandline=["xdg-open", result]))
-                items.append(item)
-            return items
+                item = Item(
+                    id=__name__,
+                    text=os.path.basename(result),
+                    subtext=result,
+                    icon=self.iconPath,
+                    actions=[
+                        Action(
+                            id="open",
+                            text="Open",
+                            callable=lambda u=f"file://{result}": openUrl(u)),
+                    ],
+                )
+                query.add(item)
