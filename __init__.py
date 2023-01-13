77b62fa8396c453de580c8b0c1c8c87ab3f0ad2b
hankliao87 <hankliao87@gmail.com>
2023-01-13 22:08:37 +0800

    Update codes

--- __init__.py
+++ __init__.py
@@ -46,24 +46,46 @@ class Plugin(QueryHandler):
 
         query_string = query.string.strip()
 
-        if len(query_string) < 3:
+        if len(query_string) < 1:
             item = Item(id=__name__, icon=self.iconPath)
-            item.text = "Enter more than three characters to search"
+            item.text = "Enter more than one characters to search"
             item.subtext = ""
             query.add(item)
-            return
         else:
             try:
                 proc_result = subprocess.run(['fd', '-L', query_string, '--max-results', str(SEARCH_LIMIT), '/home'], stdout=subprocess.PIPE, text=True, timeout=10)
 
                 results = proc_result.stdout
                 results = results.splitlines()
+                if len(results) == 0:
+                    item = Item(id=__name__, icon=self.iconPath)
+                    item.text = "Not Found"
+                    item.subtext = query_string
+                    query.add(item)
+                    return
+
+                for result in results:
+                    result = result[:-1] if result.endswith('/') else result
+                    item = Item(
+                        id=__name__,
+                        text=os.path.basename(result),
+                        subtext=result,
+                        icon=self.iconPath,
+                        actions=[
+                            Action(
+                                id="open",
+                                text="Open",
+                                callable=lambda u=f"file://{result}": openUrl(u)),
+                        ],
+                    )
+                    query.add(item)
+
             except subprocess.TimeoutExpired as ex:
                 item = Item(id=__name__, icon=self.iconPath)
                 item.text = "Timeout"
                 item.subtext = "Error: " + str(ex)
                 query.add(item)
-                return
+
             except Exception as ex:
                 item = Item(
                     id=__name__,
@@ -82,27 +104,4 @@ class Plugin(QueryHandler):
                 item.text = "Error: " + str(ex)
                 item.subtext = f"Please create an issue in {md_url}"
                 query.add(item)
-                return
 
-            if len(results) == 0:
-                item = Item(id=__name__, icon=self.iconPath)
-                item.text = "Not Found"
-                item.subtext = query_string
-                query.add(item)
-                return
-
-            for result in results:
-                result = result[:-1] if result.endswith('/') else result
-                item = Item(
-                    id=__name__,
-                    text=os.path.basename(result),
-                    subtext=result,
-                    icon=self.iconPath,
-                    actions=[
-                        Action(
-                            id="open",
-                            text="Open",
-                            callable=lambda u=f"file://{result}": openUrl(u)),
-                    ],
-                )
-                query.add(item)
